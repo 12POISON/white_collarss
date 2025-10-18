89930a37f8ff2ff266a4aa6884d94faf12f691e2
REDDY <bharadwajareddydevireddy@gmail.com>
2025-10-18 10:15:55 +0530

    Update configuration files for Vercel deployment

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Job = require('../models/job');
+const Job = require('./models/job');
 
 // Jobs listing page
 router.get('/', async (req, res) => {
